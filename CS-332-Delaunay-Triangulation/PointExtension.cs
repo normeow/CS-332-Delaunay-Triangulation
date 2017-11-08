98adcaf0833d284963150ddea0f88ad10b64e7d9
normeow <normeow@yandex.ru>
2017-11-08 16:06:05 +0300

    add DotProduct and Substruct as Point extension methods

--- CS-332-Delaunay-Triangulation/PointExtension.cs
+++ CS-332-Delaunay-Triangulation/PointExtension.cs
@@ -23,5 +23,15 @@ namespace CS_332_Delaunay_Triangulation
         {
             return Math.Sqrt(Math.Pow(point.X, 2) + Math.Pow(point.Y, 2));
         }
+
+        public static double DotProduct(this Point a, Point b)
+        {
+            return a.X * b.X + a.Y * b.Y;
+        }
+
+        public static Point Substract(this Point point, Point other)
+        {
+            return new Point(point.X - other.X, point.Y - other.Y);
+        }
     }
 }
