557b5a5b701ace38aa3cd224ad457cde9021e1f7
normeow <normeow@yandex.ru>
2017-11-10 13:28:48 +0300

    add Intersect, Rotate & Flip in Edge class

---
+++ CS-332-Delaunay-Triangulation/Geometry/Edge.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_332_Delaunay_Triangulation.Geometry
+{
+    public class Edge : IComparable<Edge>
+    {
+        public PointF Origin { get; private set; }
+        public PointF Destination { get; private set; }
+
+        public Edge(PointF origin, PointF destination)
+        {
+            this.Origin = origin;
+            this.Destination = destination;
+        }
+
+        public int CompareTo(Edge other)
+        {
+            if (Origin.LessThen(other.Origin)) return -1;
+            if (Origin.GreaterThen(other.Origin)) return 1;
+            if (Destination.LessThen(other.Destination)) return -1;
+            if (Destination.GreaterThen(other.Destination)) return 1;
+            return 0;
+        }
+
+        public void Rotate()
+        {
+            // rotate on 90
+            var mid = (Origin.Add(Destination)).Multiply(0.5f);
+            var v = Destination.Substract(Origin);
+            var n = new PointF(v.Y, -v.X);
+            Origin = mid.Substract(n.Multiply(0.5f));
+            Destination = mid.Add(n.Multiply(0.5f));
+        }
+
+        public void Flip()
+        {
+            Rotate();
+            Rotate();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>paramter of intersection equation. NaN if no intersection</returns>
+        public double Intersect(Edge other)
+        {
+            var t = Double.NaN;
+            PointF a = Origin;
+            PointF b = Destination;
+            PointF c = other.Origin;
+            PointF d = other.Destination;
+            PointF n = new PointF(d.Substract(c).Y, c.Substract(d).X);
+
+            var denom = n.DotProduct(b.Substract(a));
+            if (denom != .0)
+            {
+                var num = n.DotProduct(a.Substract(c));
+                t = -num / denom;
+            }
+
+            return t;
+        }
+    }
+}
---
+++ CS-332-Delaunay-Triangulation/Geometry/PointExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CS_332_Delaunay_Triangulation.Geometry
+{
+    public static class PointExtension
+    {
+        public static bool LessThen(this PointF point, PointF other)
+        {
+            return RadiusVector(point) < RadiusVector(other);
+        }
+
+        public static bool GreaterThen(this PointF point, PointF other)
+        {
+            return RadiusVector(point) > RadiusVector(other);
+        }
+
+        public static double RadiusVector(this PointF point)
+        {
+            return Math.Sqrt(Math.Pow(point.X, 2) + Math.Pow(point.Y, 2));
+        }
+
+        public static double DotProduct(this PointF a, PointF b)
+        {
+            return a.X * b.X + a.Y * b.Y;
+        }
+
+        public static PointF Substract(this PointF point, PointF other)
+        {
+            return new PointF(point.X - other.X, point.Y - other.Y);
+        }
+
+        public static PointF Add(this PointF point, PointF other)
+        {
+            return new PointF(point.X + other.X, point.Y + other.Y);
+        }
+
+        public static PointF Multiply(this PointF point, float x)
+        {
+            return new PointF(point.X * x, point.Y * x);
+        }
+
+    }
+
+}
